29b210afe401f36617606720999f179d03358af7
Emilio Ramirez <emilioramirezeguia@gmail.com>
2020-09-09 08:52:23 -0700

    Implement Stack class with LinkedList

--- stack/stack.py
+++ stack/stack.py
@@ -12,20 +12,42 @@ return elements in Last In First Out order.
 """
 
 
+from singly_linked_list import LinkedList
+
+
+# class Stack:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+
+#     def __len__(self):
+#         return self.size
+
+#     def push(self, value):
+#         self.storage.append(value)
+#         self.size += 1
+
+#     def pop(self):
+#         if self.size == 0:
+#             return None
+#         self.size -= 1
+#         return self.storage.pop()
+
+
 class Stack:
     def __init__(self):
         self.size = 0
-        self.storage = []
+        self.storage = LinkedList()
 
     def __len__(self):
         return self.size
 
     def push(self, value):
+        self.storage.add_to_tail(value)
         self.size += 1
-        self.storage.append(value)
 
     def pop(self):
         if self.size == 0:
             return None
         self.size -= 1
-        return self.storage.pop()
+        return self.storage.remove_tail()
